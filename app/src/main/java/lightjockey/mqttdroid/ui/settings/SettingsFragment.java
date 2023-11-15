a042da33e08d332d71e77f567f72be0f4d571500
LightJockey <datlightingdude@gmail.com>
2023-11-15 16:40:39 +0100

    Refactor clickable prefs

--- app/src/main/java/lightjockey/mqttdroid/ui/settings/SettingsFragment.java
+++ app/src/main/java/lightjockey/mqttdroid/ui/settings/SettingsFragment.java
@@ -3,6 +3,7 @@ package lightjockey.mqttdroid.ui.settings;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -21,35 +22,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 
         updateMqttAuthPrefs();
 
-        Preference clickablePref = findPreference(MqttDroidApp.appContext.getString(R.string.pref_key_backup_save));
-        if (clickablePref != null) {
-            clickablePref.setOnPreferenceClickListener(pref -> {
-                Utils.backupData(getContext());
-                return true;
-            });
-        }
-        clickablePref = findPreference(MqttDroidApp.appContext.getString(R.string.pref_key_backup_restore));
-        if (clickablePref != null) {
-            clickablePref.setOnPreferenceClickListener(pref -> {
-                Utils.restoreData(getContext());
-                return true;
-            });
-        }
-        clickablePref = findPreference(MqttDroidApp.appContext.getString(R.string.pref_key_mqtt_broker_test));
-        if (clickablePref != null) {
-            clickablePref.setOnPreferenceClickListener(pref -> {
-                Utils.testBrokerConnection(getContext());
-                return true;
-            });
-        }
-        clickablePref = findPreference(MqttDroidApp.appContext.getString(R.string.pref_key_info_github));
-        if (clickablePref != null) {
-            clickablePref.setSummary(MqttDroidApp.appContext.getString(R.string.pref_sum_info_github, BuildConfig.COMMIT_HASH));
-            clickablePref.setOnPreferenceClickListener(pref -> {
-                Utils.startUrlIntent("https://github.com/LightJockey/MqttDroid");
-                return true;
-            });
-        }
+        bindClickablePrefs();
     }
 
     @Override
@@ -64,6 +37,12 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
             updateMqttAuthPrefs();
     }
 
+    private static void setPrefEnabled(Preference pref, boolean enabled) {
+        if (pref == null) return;
+        pref.setEnabled(enabled);
+        pref.setShouldDisableView(!enabled);
+    }
+
     private void updateMqttAuthPrefs() {
         boolean useAuth = MqttDroidApp.sharedPrefs.getBoolean(MqttDroidApp.appContext.getString(R.string.pref_key_mqtt_client_use_auth), false);
         Preference usernamePref = findPreference(MqttDroidApp.appContext.getString(R.string.pref_key_mqtt_client_auth_username));
@@ -72,9 +51,25 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
         setPrefEnabled(passwordPref, useAuth);
     }
 
-    private static void setPrefEnabled(Preference pref, boolean enabled) {
-        if (pref == null) return;
-        pref.setEnabled(enabled);
-        pref.setShouldDisableView(!enabled);
+    private void bindClickablePrefs() {
+        buildClickablePref(R.string.pref_key_backup_save, (pref) -> { Utils.backupData(getContext()); return true; });
+        buildClickablePref(R.string.pref_key_backup_restore, (pref) -> { Utils.restoreData(getContext()); return true; });
+        buildClickablePref(R.string.pref_key_mqtt_broker_test, (pref) -> { Utils.testBrokerConnection(getContext()); return true; });
+
+        Preference githubPref = findPreference(MqttDroidApp.appContext.getString(R.string.pref_key_info_github));
+        if (githubPref != null) {
+            githubPref.setSummary(MqttDroidApp.appContext.getString(R.string.pref_sum_info_github, BuildConfig.COMMIT_HASH));
+            buildClickablePref(githubPref, (pref) -> { Utils.startUrlIntent("https://github.com/LightJockey/MqttDroid"); return true; });
+        }
+    }
+
+    private void buildClickablePref(int prefKey, Preference.OnPreferenceClickListener onClickHandler) {
+        Preference pref = findPreference(MqttDroidApp.appContext.getString(prefKey));
+        buildClickablePref(pref, onClickHandler);
+    }
+    private void buildClickablePref(Preference pref, Preference.OnPreferenceClickListener onClickHandler) {
+        if (pref == null)
+            return;
+        pref.setOnPreferenceClickListener(onClickHandler);
     }
 }
